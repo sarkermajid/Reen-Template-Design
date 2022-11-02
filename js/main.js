a85b5f6451fcc114c9ef7fc75fd8d713d158a73e
Md. Majidur Rahman Sarker <63114252+sarkermajid@users.noreply.github.com>
2022-11-02 22:29:11 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $("#owl-demo").owlCarousel({
+
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        loop : true,
+        items : 4,
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false
+
+    });
+
+});
